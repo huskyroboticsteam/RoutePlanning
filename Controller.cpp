36b83ac8a831337b54ec4652388a3d16b15ef90a
Gary Geng <garygengxiao@gmail.com>
2018-11-06 22:51:13 -0800

    Add/modify skeletons for Controller, Server, Agent; merge Map and Agent into Agent

---
+++ Controller.cpp
@@ -0,0 +1,4 @@
+#include "Controller.h"
+#include "Agent.h"
+
+RoverPathfinding::Controller::Controller(RoverPathfinding::Agent agt, RoverPathfinding::Server serv): agent(agt), server(serv) {}
--- Controller.h
+++ Controller.h
@@ -1,22 +1,29 @@
 /*
-Controller class for decoding input from obstacle detection and sensors
-to be used for computations in Map and Agent, and encoding the output
+Controller class for getting the input from obstacle detection and sensors
+to be used for computations in  Agent, and passing the output to the server
 (i.e. suggested direction, speed, etc.) to be fed back to the drivetrain.
+Basically this class handles everything: model/logic (Agent), networking
+(Server), and view.
 */
 
 #ifndef ROVERPATHFINDING_CONTROLLER_H
 #define ROVERPATHFINDING_CONTROLLER_H
 
-#include "Map.h"
 #include "Agent.h"
+#include "Server.h"
 
 namespace RoverPathfinding
 {
 class Controller
 {
+  public:
+    Controller(RoverPathfinding::Agent agt, RoverPathfinding::Server serv);
   private:
-    RoverPathfinding::Map map;
-    RoverPathfinding::Agent agent;
+    // TODO might take some parameters
+    void update();                    // Update the current world state.
+
+    RoverPathfinding::Agent agent;    // The agent that handles the logic of finding a path         
+    RoverPathfinding::Server server;  // The server that handles networking and triggers
 };
 } // namespace RoverPathfinding
 
