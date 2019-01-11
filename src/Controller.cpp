8654e5f6f47d5df012f271ac2d54131343bb98c1
kinjal <kinjalh@github.com>
2019-01-10 19:42:03 -0800

    irrelevant commit

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -6,21 +6,23 @@
 #define DATA_MAG 0xC1
 
 #define TARGET_LAT 123.0
-#define TARGET_LONGITUDE 321.0
+#define TARGET_LNG 321.0
 
 namespace RoverPathfinding {
     RoverPathfinding::Map map;
+
     // TODO: split packet into separate parts (timestamp, packetID, data)
     // TODO: Feed lat/long to map to find vector pointing to next destination
     // TODO: Feed x/y/z into ??? to find current orientation, then we can find how much to turn
+    
     // using given packet data and server send a packet containing either a direction or motor power
     void parsePacket(unsigned char packetID, unsigned char data[]) {
         // GPS has latitude and longitude
         if (packetID == DATA_GPS) {
             float lat = 0.0;
             std::memcpy(&lat, data, sizeof(float));
-            float longitude = 0.0;
-            std::memcpy(&longitude, &data[sizeof(float)], sizeof(float));
+            float lng = 0.0;
+            std::memcpy(&lng, &data[sizeof(float)], sizeof(float));
         // Magnometer has x, y, z values
         } else if (packetID == DATA_MAG) {
             float x = 0.0, y = 0.0, z = 0.0;
@@ -29,5 +31,7 @@ namespace RoverPathfinding {
             std::memcpy(&z, &data[2 * sizeof(float)], sizeof(float));
         }
     }
+
+
 }
 
