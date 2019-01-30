397d9a2836f375dd4928c24e58f2be3470ca3087
Andrew Briand <atb8888@comcast.net>
2019-01-29 18:53:40 -0800

    Fixed Controller

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -76,9 +76,9 @@ namespace RP {
     // angle must be in radians, dist in meters
     // formula source: stackoverflow q 53182179 (convert lat/long to XY); I simply did the reverse math
     point Controller::convertToLatLng(float dist, float angle) {
-		return RP::Controller::converToLatLng(curr_lat, curr_lng, curr_dir, dist, angle); 
+		return converToLatLng(curr_lat, curr_lng, curr_dir, dist, angle); 
     }
-	point Controller::convertToLatLng(float lat, float lng, float dir,float dist, float angle) {
+	static point Controller::convertToLatLng(float lat, float lng, float dir, float dist, float angle) {
 		float delta_x = dist * cos(angle + dir + M_PI/2);
 		float delta_y = dist * sin(angle + dir + M_PI/2);
 		//std::cout << "delta_x: " << delta_x << " delta_y: " << delta_y << "\n";
--- src/Controller.hpp
+++ src/Controller.hpp
@@ -14,7 +14,7 @@ namespace RP
 			void parsePacket(unsigned char packetID, unsigned char data[]);
             void addObstacle(float dist1, float dir1, float dist2, float dir2);
 			void foundTennisBall(float dist, float dir);
-			
+			static point convertToLatLng(float lat, float lng, float dir, float dist, float angle);
         private:
             point convertToLatLng(float dist, float dir);
 			float curr_lat, curr_lng, curr_dir;
