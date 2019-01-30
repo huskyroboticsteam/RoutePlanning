9002f7510fa4a53ad6258fef4743e3653ae5ef2b
pfortet <pfortet@cs.washington.edu>
2019-01-29 20:10:12 -0800

    in progress

---
+++ RobotSim/RobotSim/gui.cpp
@@ -0,0 +1,9 @@
+//
+//  gui.cpp
+//  RobotSim
+//
+//  Created by Tadeusz Pforte on 1/29/19.
+//  Copyright © 2019 Husky Robotics. All rights reserved.
+//
+
+#include "gui.hpp"
---
+++ RobotSim/RobotSim/gui.hpp
@@ -0,0 +1,16 @@
+//
+//  gui.hpp
+//  RobotSim
+//
+//  Created by Tadeusz Pforte on 1/29/19.
+//  Copyright © 2019 Husky Robotics. All rights reserved.
+//
+
+#ifndef gui_hpp
+#define gui_hpp
+
+#include <stdio.h>
+
+
+
+#endif /* gui_hpp */
