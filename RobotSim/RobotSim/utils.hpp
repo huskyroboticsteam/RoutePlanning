6ce3e6bb3d87be4b54530fcc76d21e6bc5f85c7b
Gary Geng <garygengxiao@gmail.com>
2019-02-19 19:03:48 -0800

    Update themes

--- RobotSim/RobotSim/utils.hpp
+++ RobotSim/RobotSim/utils.hpp
@@ -6,7 +6,21 @@
 #include <SFML/Graphics.hpp>
 // #include "Map.hpp"
 
-#define THEME 1 // 0 is normal, 1 is HACKERMODE
+#define THEME 0 // 0 is normal, 1 is HACKERMODE
+#define TARGET_COLOR sf::Color:Red;
+#if THEME
+#define PATH_COLOR sf::Color::Yellow 
+#define OBST_COLOR sf::Color(82, 165, 46)
+#define VISIBLE_OBST_COLOR sf::Color(178, 255, 145)
+#define SEEN_OBST_COLOR sf::Color::Green
+#define VIEW_SHAPE_COLOR sf::Color::White
+#else
+#define PATH_COLOR sf::Color(115, 92, 196);
+#define OBST_COLOR sf::Color::Black
+#define VISIBLE_OBST_COLOR sf::Color::Green
+#define SEEN_OBST_COLOR sf::Color(58, 201, 0)
+#define VIEW_SHAPE_COLOR sf::Color::Blue
+#endif
 //#define float double // YOU ARE GOING TO HELL
  
 typedef unsigned int uint;
