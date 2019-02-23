738198259db9f48085c7accb6c528462b8ff43c7
Andrew Briand <atb8888@comcast.net>
2019-02-23 11:27:03 -0800

    Started World Communicator

---
+++ RobotSim/RobotSim/WorldCommunicator.hpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h> 
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#define SOCKET int
+#define SOCKET_ERROR -1
+
+class WorldCommunicator {
+	public:
+		// Sends and receives packets
+		// To be called in the main update loop
+		void update();
+		WorldCommunicator();
+	private:
+		int timer;
+		bool send_action(std::vector<unsigned char> data, unsigned char id);
+		bool listen();
+		SOCKET out;
+		
+}
