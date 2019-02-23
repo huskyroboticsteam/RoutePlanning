f5b5381b1b30812c1152b9363bc7654f28b20ae2
pfortet <pfortet@cs.washington.edu>
2019-02-23 10:54:13 -0800

    comments, what a world

--- RobotSim/RobotSim/main.cpp
+++ RobotSim/RobotSim/main.cpp
@@ -40,6 +40,15 @@ const std::string RESOURCE_DIR = resourcePath();
 
 const sf::Color bgColor = sf::Color(255, 255, 255);
 
+Grid grid(40.f, 40.f, 36 * WINDOW_SCALE);
+float gridScale = grid.retrieveScale();
+float gridHeight = grid.retrieveHeight();
+Agent agent(gridScale, grid.retrieveWidth(), gridHeight, 2.f, 2.f);
+RP::Simulator sim(grid.obstacleList, agent, RP::simulator_config{70.f, 10.f}, gridScale, gridHeight);
+RP::Map map(sim.getpos(), grid.target);
+
+RP::AutoController control(grid, agent, map);
+
 int main(int, char const **)
 {
     const unsigned int FRAMERATE = 60;
@@ -61,17 +70,11 @@ int main(int, char const **)
     {
         window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
     }
-
-    Grid grid(40.f, 40.f, 36 * WINDOW_SCALE);
-    float gridScale = grid.retrieveScale();
-    float gridHeight = grid.retrieveHeight();
-    Agent agent(gridScale, grid.retrieveWidth(), gridHeight, 2.f, 2.f);
+    
+    // OPTIONAL FURTHER INIT
     agent.scaleSpeed(2.f);
     grid.target = RP::point{35.f, 35.f};
-    RP::Simulator sim(grid.obstacleList, agent, RP::simulator_config{70.f, 10.f}, gridScale, gridHeight);
-    RP::Map map(sim.getpos(), grid.target);
-
-    RP::AutoController control(grid, agent, map);
+    // END INIT
 
     while (window.isOpen()) {
         sf::Event event;
@@ -83,58 +86,58 @@ int main(int, char const **)
             else if (event.type == sf::Event::KeyPressed)
             {
                 switch (event.key.code) {
-                case sf::Keyboard::H : {
-                    std::cout << "Help Menu: " << std::endl;
-                    std::cout << "P   -- Returns the internal position of the robot" << std::endl;
-                    std::cout << "G   -- Toggle grid" << std::endl;
-                    std::cout << "O   -- Import obstacles from obstacles.txt" << std::endl;
-                    std::cout << "U   -- Complete autonomous mode" << std::endl;
-                    std::cout << "N   -- Toggle clipping" << std::endl;
-                    std::cout << "0   -- Toggle robot path" << std::endl;
-                    std::cout << "9   -- Draw algorithm path" << std::endl;
-                    break;
-                }
-                case sf::Keyboard::P : {
-                    std::cout << "Internal Position: (" << agent.getX() << "," << agent.getY()
-                              << ") at " << agent.getInternalRotation() << " degrees" << std::endl;
-                    break;
-                }
-                case sf::Keyboard::G : {
-                    grid.toggleGrid();
-                    break;
-                }
-                case sf::Keyboard::O : {
-                    grid.obstacleList.clear();
-                    grid.readObstaclesFromFile(RESOURCE_DIR + "obstacles.txt");
-                    // grid.addBorderObstacles();
-                    std::cout << "Added obstacles" << std::endl;
-                    break;
-                }
-                case sf::Keyboard::U : {
-                    auton = !auton;
-                    if (auton)
-                        control.start_auto();
-                    else
-                        control.stop_auto();
-                    break;
-                }
-                case sf::Keyboard::N : {
-                    grid.toggleClipping();
-                    break;
-                }
-                case sf::Keyboard::Num9 : {
-                    if (lazer)
-                        grid.drawPath();
-                    lazer = !lazer;
-                    break;
-                }
-                case sf::Keyboard::Num0 : {
-                    agent.togglePath();
-                    break;
-                }
-                case sf::Keyboard::B : {
-                    map.breakpoint(); // temp; used for debug only
-                }
+                    case sf::Keyboard::H : {
+                        std::cout << "Help Menu: " << std::endl;
+                        std::cout << "P   -- Returns the internal position of the robot" << std::endl;
+                        std::cout << "G   -- Toggle grid" << std::endl;
+                        std::cout << "O   -- Import obstacles from obstacles.txt" << std::endl;
+                        std::cout << "U   -- Complete autonomous mode" << std::endl;
+                        std::cout << "N   -- Toggle clipping" << std::endl;
+                        std::cout << "0   -- Toggle robot path" << std::endl;
+                        std::cout << "9   -- Draw algorithm path" << std::endl;
+                        break;
+                    }
+                    case sf::Keyboard::P : {
+                        std::cout << "Internal Position: (" << agent.getX() << "," << agent.getY()
+                                  << ") at " << agent.getInternalRotation() << " degrees" << std::endl;
+                        break;
+                    }
+                    case sf::Keyboard::G : {
+                        grid.toggleGrid();
+                        break;
+                    }
+                    case sf::Keyboard::O : {
+                        grid.obstacleList.clear();
+                        grid.readObstaclesFromFile(RESOURCE_DIR + "obstacles.txt");
+                        // grid.addBorderObstacles();
+                        std::cout << "Added obstacles" << std::endl;
+                        break;
+                    }
+                    case sf::Keyboard::U : {
+                        auton = !auton;
+                        if (auton)
+                            control.start_auto();
+                        else
+                            control.stop_auto();
+                        break;
+                    }
+                    case sf::Keyboard::N : {
+                        grid.toggleClipping();
+                        break;
+                    }
+                    case sf::Keyboard::Num9 : {
+                        if (lazer)
+                            grid.drawPath();
+                        lazer = !lazer;
+                        break;
+                    }
+                    case sf::Keyboard::Num0 : {
+                        agent.togglePath();
+                        break;
+                    }
+                    default : {
+                        std::cout << "Command not recognized" << std::endl;
+                    }
                 }
             }
         }
@@ -160,8 +163,32 @@ int main(int, char const **)
     return EXIT_SUCCESS;
 }
 
-static void move(float x, float y);
-static void turn(float rotation);
-static RP::point currentPosition();
-static float currentRotation();
-static std::vector<RP::line> currentObstaclesInView();
+// moves the robot forward or backward at a given speed
+// speed should be between 1 and -1, where negative is backwards
+static void move(float speed) {
+    grid.moveAgent(agent, speed);
+}
+
+// rotates the robot clockwise or counterclockwise at a given speed
+// speed should be between 1 and -1, where negative is clockwise
+static void turn(float speed) {
+    grid.rotateAgent(agent, speed);
+}
+
+// returns the current position of the robot, in meters, relative to the grid origin
+static RP::point currentPosition() {
+    return RP::point{ agent.getX(), agent.getY() };
+}
+
+// returns the current rotation of the robot, in degrees increasing counterclockwise
+// value is between 0 and 360, where 0 is true right relative to the grid
+static float currentRotation() {
+    return agent.getInternalRotation();
+}
+
+// returns all the obstacles currently visible to the robot
+// note that these may be partial obstacles
+// TODO may return an RP::obstacle depending on implementation
+static std::vector<RP::line> currentObstaclesInView() {
+    // TODO
+}
