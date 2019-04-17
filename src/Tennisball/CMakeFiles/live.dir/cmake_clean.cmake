63e824d4dde10970b33d76e55e645d3fa19173b5
Andrew Briand <atb8888@comcast.net>
2019-04-16 19:28:33 -0700

    added tennisball folder

---
+++ src/Tennisball/CMakeFiles/live.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/live.dir/src/live_video.cpp.o"
+  "CMakeFiles/live.dir/src/detector.cpp.o"
+  "bin/live.pdb"
+  "bin/live"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/live.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Tennisball/CMakeFiles/still.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/still.dir/src/still_image.cpp.o"
+  "CMakeFiles/still.dir/src/detector.cpp.o"
+  "bin/still.pdb"
+  "bin/still"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/still.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
